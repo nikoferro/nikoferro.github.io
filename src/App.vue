5afb78495e642c821d1a90d46a22b27231c84ab5
Nicolas Ferro <nicolaspatricioferro@gmail.com>
2017-12-31 13:40:53 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import "font-awesome/css/font-awesome.css";
+import Header from "./components/Header";
+
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  padding: 0px;
+  margin: 0px;
+}
+#app {
+  height: 100%;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+      <div>Hi <span>👋</span></div>
+      <h1>I’m {{name}}</h1>
+      <div>{{msg}}</div>
+      <ul id="social">
+        <li v-bind:key="s.icon" v-for="s in social">
+          <a :href="s.url"><i :class="'fa fa-'+s.icon" aria-hidden="true"></i></a>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      name: "Nicolas Ferro",
+      msg: "A developer from Buenos Aires, Argentina",
+      social: [
+        { icon: "github", url: "https://github.com/nikoferro" },
+        { icon: "linkedin", url: "https://github.com/nikoferro" }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="css" scopped>
+#social {
+  margin: 0px;
+  padding: 0px;
+}
+#social li {
+  list-style: none;
+  display: inline-block;
+  margin: 1em;
+  font-size: 24px;
+}
+#social li a {
+  color: #2c3e50;
+  transition: opacity 0.2s;
+}
+#social li a:hover {
+  opacity: 0.5;
+}
+</style>
